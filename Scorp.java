1373236c00b741d19014c13db278974b8ac10394
kayak28 <kaya_ota@msn.com>
2013-03-22 20:51:21 -0700

    having error: prb is not lifetime?

--- Scorp.java
+++ Scorp.java
@@ -1,10 +1,15 @@
 
 public class Scorp
 {
-	int x = 555;
-	 static void printX()
+	 int x = 555;
+	 void printX()
 	 {
 		 System.out.println("print x"); 
 		 System.out.printf("x = ", x);
+	 }
+	 public static void main(String [] args)
+	 {
+		 System.out.printf("main: x = %d", x);
+		 //ERROR: can not make a static reference to the non-static field x 
 	 }
 }
